5ac8b01ac741c323d5f1bd178325e9dab1bbfcfa
luizalexandrew <luiz.alexandre@live.com>
2019-11-22 17:49:12 -0300

    Desativando a instalação do node

--- node.sh
+++ node.sh
@@ -1,35 +1,35 @@
-#!/bin/bash
+# #!/bin/bash
 
-#https://www.digitalocean.com/community/tutorials/how-to-install-node-js-on-debian-10
+# #https://www.digitalocean.com/community/tutorials/how-to-install-node-js-on-debian-10
 
-curl -sL https://deb.nodesource.com/setup_12.x -o nodesource_setup.sh
-chmod +x nodesource_setup.sh
-bash nodesource_setup.sh
-apt install nodejs
-node -v
-apt install build-essential
+# curl -sL https://deb.nodesource.com/setup_12.x -o nodesource_setup.sh
+# chmod +x nodesource_setup.sh
+# bash nodesource_setup.sh
+# apt install nodejs
+# node -v
+# apt install build-essential
 
-curl -sL https://raw.githubusercontent.com/nvm-sh/nvm/v0.34.0/install.sh -o install_nvm.sh
-bash install_nvm.sh
-source ~/.profile
+# curl -sL https://raw.githubusercontent.com/nvm-sh/nvm/v0.34.0/install.sh -o install_nvm.sh
+# bash install_nvm.sh
+# source ~/.profile
 
-rm -r nodesource_setup.sh
-rm -r install_nvm.sh
+# rm -r nodesource_setup.sh
+# rm -r install_nvm.sh
 
-clear
-nvm install 12.13.1
-nvm use 12.13.1
+# clear
+# nvm install 12.13.1
+# nvm use 12.13.1
 
-echo "###### PARA MUDAR NO SEU USUARIO #################"
-echo "nvm use 10.16.2"
-echo "###### PARA ESCOLHER OUTRA DE VERSAO ############"
-echo "nvm ls-remote"
-echo "nvm install 10.16.2"
-echo "nvm use 10.16.2"
-echo "node -v"
-echo "##################################################"
-echo "######## Aperte ENTER para continuar #############"
-echo "SÓ CONTINUE QUANDO ESCOLHER O NODE NO SEU USUÁRIO"
-echo "##################################################"
+# echo "###### PARA MUDAR NO SEU USUARIO #################"
+# echo "nvm use 10.16.2"
+# echo "###### PARA ESCOLHER OUTRA DE VERSAO ############"
+# echo "nvm ls-remote"
+# echo "nvm install 10.16.2"
+# echo "nvm use 10.16.2"
+# echo "node -v"
+# echo "##################################################"
+# echo "######## Aperte ENTER para continuar #############"
+# echo "SÓ CONTINUE QUANDO ESCOLHER O NODE NO SEU USUÁRIO"
+# echo "##################################################"
 
-read WAITNODE
+# read WAITNODE
